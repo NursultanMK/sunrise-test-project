cfd825a7ee626ea65e2601b51f720ae46f1ed8a4
Nursultan Muratov <shamanws1992@gmail.com>
2018-09-04 05:17:08 +0600

    create autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('app/dev/scss/main.scss')
@@ -12,3 +13,12 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
     gulp.watch('app/dev/scss/**/*.scss', ['sass']);
 });
+
+gulp.task('autoprefix', function () {
+    gulp.src('app/dev/css/main.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/dev/css'))
+})
